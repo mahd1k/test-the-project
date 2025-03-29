70c642e4327f76fe1fbaaf90fea55d027edf4ae6
mahdi <madlhi123789@gmail.com>
2025-03-29 13:39:02 +0330

    first maseage

---
+++ mamad.js
@@ -0,0 +1,9 @@
+/* var mn = 2
+while(mn<=100){
+    console.log(mn);
+   mn++;
+} */
+var first = document.getElementById('aval')
+var second = document.getElementById('dovom')
+var result = document.getElementById('result')
+var computebtn = document.getElementById('compute')
